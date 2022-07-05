ccc658c13ccd6862fd74928f3d5b2608dc404e67
connor-humphries <105638822+connor-humphries@users.noreply.github.com>
2022-07-05 15:06:07 +0100

    Add files via upload

---
+++ db_setup_script.sql
@@ -0,0 +1,50 @@
+CREATE table IF NOT EXISTS customers (
+	customer_id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
+);
+
+CREATE table IF NOT EXISTS products (
+	product_id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
+);
+
+CREATE table IF NOT EXISTS store (
+	store_id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(255) NOT NULL
+);
+
+CREATE table IF NOT EXISTS cards (
+	card_id INT AUTO_INCREMENT PRIMARY KEY,
+	card_number BIGINT NOT NULL,
+	card_type VARCHAR(255) NOT NULL
+);
+
+CREATE table IF NOT EXISTS cards_customers (
+	customer_id INT NOT NULL,
+	card_id INT NOT NULL,
+	FOREIGN KEY (customer_id) 
+		REFERENCES customers (customer_id),
+	FOREIGN KEY (card_id) 
+		REFERENCES cards (card_id)
+);
+
+CREATE table IF NOT EXISTS transactions (
+	transaction_id INT AUTO_INCREMENT PRIMARY KEY,
+	date_time DATETIME NOT NULL,
+	customer_id INT NOT NULL,
+	product_id INT NOT NULL,
+	quantity INT NOT NULL,
+	store_id INT NOT NULL,
+	total DECIMAL(10,2) NOT NULL,
+	payment_method VARCHAR(255) DEFAULT 'CASH',
+	
+	FOREIGN KEY (customer_id)
+		REFERENCES customers (customer_id),
+	FOREIGN KEY (product_id)
+		REFERENCES products (product_id),
+	FOREIGN KEY (store_id)
+		REFERENCES store (store_id)
+);
+	
+	
+		
