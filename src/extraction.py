70871f475f2cebbf888e668dd7e99800aa52464d
Connor Humphries <connor.humphries@sainsburys.co.uk>
2022-07-06 11:35:59 +0100

    pandas profiling report

--- src/extraction.py
+++ src/extraction.py
@@ -3,12 +3,16 @@ import pandas_profiling
 import os 
 
 def get_data_frame():
-    df = pd.DataFrame(
-        columns = ['DATE', 'LOCATION', 'CUSTOMER NAME', 'PRODUCTS', 'PAYMENT METHOD', 'TOTAL', 'CARD'])
+    df = pd.DataFrame()
     for filename in os.listdir('../data'):
         temp_df = pd.read_csv(f'../data/{filename}')
         df = pd.concat([df, temp_df], axis=0)
     return df
 
 df = get_data_frame()
+df.columns = ['DATE', 'LOCATION', 'CUSTOMER NAME', 'PRODUCTS', 'PAYMENT METHOD', 'TOTAL', 'CARD']
+df.reset_index()
 print(df.shape)
+print(df.sample(5))
+
+
