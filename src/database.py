ad082547bc337576840402b4dbbc2e17c3d8df89
Connor Humphries <connor.humphries@sainsburys.co.uk>
2022-07-06 16:28:35 +0100

    uploading to db funcs

--- src/database.py
+++ src/database.py
@@ -1,9 +1,8 @@
-import dotenv
+from dotenv import load_dotenv
 import pymysql
 import os
 
-
-dotenv.load_dotenv()
+load_dotenv()
 host = os.environ.get("mysql_host")
 user = os.environ.get("mysql_user")
 password = os.environ.get("mysql_pass")
@@ -12,19 +11,16 @@ warehouse_db_name = os.environ.get("mysql_db")
 
 def get_connection():
     connection = pymysql.connect(
-    host=host,
-    user=user,
-    password=password,
-    database=warehouse_db_name
+        host=host, user=user, password=password, database=warehouse_db_name
     )
     return connection
 
 
-def close_connection (connection, cursor=None):
+def close_connection(connection, cursor=None):
     if cursor is not None:
         cursor.close()
     connection.close()
 
 
-def commit_connection (connection):
+def commit_connection(connection):
     connection.commit()
--- src/extraction.py
+++ src/extraction.py
@@ -1,10 +1,15 @@
 import pandas as pd
 import pandas_profiling
 import os
+from database import get_connection, close_connection, commit_connection
+from dotenv import load_dotenv
+from yaspin import yaspin
+import time
 
+connection = get_connection()
 # This function forms the **E** from ETL - it extracts the data and puts it into a dataframe.
 
-
+@yaspin(text='Cleaning data...')
 def get_data_frame():
     df = pd.DataFrame()
     for filename in os.listdir("../data"):
@@ -21,7 +26,7 @@ def get_data_frame():
     ]
     df.reset_index()
     # This part of the function begins to work on the transform
-    df[["card_type", "card-number"]] = df["card"].str.split(",", expand=True)
+    df[["card_type", "card_number"]] = df["card"].str.split(",", expand=True)
     df.dropna()
     return df
 
@@ -29,17 +34,12 @@ def get_data_frame():
 # Gets the data frame to check the code is working
 df = get_data_frame()
 
-'''
+"""
 WE NEED:
-into CUSTOMERS:
-- Customer Name
 
 into CARDS:
 - number
 - type
-
-into STORE:
-- name
 
 into TRANSACTION:
 - timestamp
@@ -50,8 +50,43 @@ into TRANSACTION:
 
 into PRODUCTS:
 - name
-'''
-for name in df['customer_name']:
-    sql_query = f'''
-    INSERT into customers (customer_name)
-        VALUES ('{name}')'''
+"""
+
+
+@yaspin(text='Inserting names into DB...')
+def insert_names(connection):
+    for name in df["customer_name"]:
+        sql_query = f"""
+        INSERT into customers (name)
+            VALUES ('{name}')"""
+        cursor = connection.cursor()
+        cursor.execute(sql_query)
+    connection.commit()
+    print('Names inserted OK')
+
+@yaspin(text='Inserting cards into DB...')
+def insert_cards(connection):
+    for card_type in df["card_type"]:
+        for card_number in df["card_number"]:
+            sql_query = f"""
+            INSERT INTO cards (card_number, card_type)
+                VALUES ('{card_number}', '{card_type}')"""
+        cursor = connection.cursor()
+        cursor.execute(sql_query)
+        connection.commit()
+        print('Cards inserted OK')
+
+@yaspin(text='Inserting stores into DB...')
+def insert_store(connection):
+    for store in df["location"]:
+        sql_query = f"""
+        INSERT INTO store (name)
+        VALUES ('{store}') """
+        cursor = connection.cursor()
+        cursor.execute(sql_query)
+        connection.commit()
+        print('Stores inserted OK')
+
+insert_names(connection)
+insert_cards(connection)
+insert_store(connection)
