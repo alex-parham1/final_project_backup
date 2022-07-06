a2e4c98efee867149dfa58a50b6a10e7b74e8b0a
Connor Humphries <connor.humphries@sainsburys.co.uk>
2022-07-06 14:13:15 +0100

    formatted with black

--- src/extraction.py
+++ src/extraction.py
@@ -1,19 +1,30 @@
-import pandas as pd 
+import pandas as pd
 import pandas_profiling
-import os 
+import os
+
+# This function forms the **E** from ETL - it extracts the data and puts it into a dataframe.
+
 
 def get_data_frame():
     df = pd.DataFrame()
-    for filename in os.listdir('../data'):
-        temp_df = pd.read_csv(f'../data/{filename}')
+    for filename in os.listdir("../data"):
+        temp_df = pd.read_csv(f"../data/{filename}")
         df = pd.concat([df, temp_df], axis=0)
-    df.columns = ['DATE', 'LOCATION', 'CUSTOMER NAME', 'PRODUCTS', 'PAYMENT METHOD', 'TOTAL', 'CARD']
+    df.columns = [
+        "DATE",
+        "LOCATION",
+        "CUSTOMER NAME",
+        "PRODUCTS",
+        "PAYMENT METHOD",
+        "TOTAL",
+        "CARD",
+    ]
     df.reset_index()
-    
-    df[['CARD', 'CARD NUMBER']] = df['CARD'].str.split(',', expand=True)
+    # This part of the function begins to work on the transform
+    df[["CARD", "CARD NUMBER"]] = df["CARD"].str.split(",", expand=True)
     df.dropna()
     return df
 
+
+# Gets the data frame to check the code is working
 df = get_data_frame()
-
-
