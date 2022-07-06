9db4bd81cb5b808b7d94f443578ea7c8455430d6
Connor Humphries <connor.humphries@sainsburys.co.uk>
2022-07-06 15:17:40 +0100

    merging

--- src/extraction.py
+++ src/extraction.py
@@ -11,20 +11,47 @@ def get_data_frame():
         temp_df = pd.read_csv(f"../data/{filename}")
         df = pd.concat([df, temp_df], axis=0)
     df.columns = [
-        "DATE",
-        "LOCATION",
-        "CUSTOMER NAME",
-        "PRODUCTS",
-        "PAYMENT METHOD",
-        "TOTAL",
-        "CARD",
+        "date",
+        "location",
+        "customer_name",
+        "products",
+        "payment_type",
+        "total",
+        "card",
     ]
     df.reset_index()
     # This part of the function begins to work on the transform
-    df[["CARD", "CARD NUMBER"]] = df["CARD"].str.split(",", expand=True)
+    df[["card_type", "card-number"]] = df["card"].str.split(",", expand=True)
     df.dropna()
     return df
 
 
 # Gets the data frame to check the code is working
 df = get_data_frame()
+
+'''
+WE NEED:
+into CUSTOMERS:
+- Customer Name
+
+into CARDS:
+- number
+- type
+
+into STORE:
+- name
+
+into TRANSACTION:
+- timestamp
+- customer id (foreign key)
+- product id (foreign key)
+- store id (foreign key)
+- total
+
+into PRODUCTS:
+- name
+'''
+for name in df['customer_name']:
+    sql_query = f'''
+    INSERT into customers (customer_name)
+        VALUES ('{name}')'''
