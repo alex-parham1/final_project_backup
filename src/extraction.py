82b5f92d43e5a9c32b97152489db79b209ecf4d6
Connor Humphries <connor.humphries@sainsburys.co.uk>
2022-07-06 12:11:29 +0100

    data magic

--- src/extraction.py
+++ src/extraction.py
@@ -7,12 +7,13 @@ def get_data_frame():
     for filename in os.listdir('../data'):
         temp_df = pd.read_csv(f'../data/{filename}')
         df = pd.concat([df, temp_df], axis=0)
+    df.columns = ['DATE', 'LOCATION', 'CUSTOMER NAME', 'PRODUCTS', 'PAYMENT METHOD', 'TOTAL', 'CARD']
+    df.reset_index()
+    
+    df[['CARD', 'CARD NUMBER']] = df['CARD'].str.split(',', expand=True)
+    df.dropna()
     return df
 
 df = get_data_frame()
-df.columns = ['DATE', 'LOCATION', 'CUSTOMER NAME', 'PRODUCTS', 'PAYMENT METHOD', 'TOTAL', 'CARD']
-df.reset_index()
-print(df.shape)
-print(df.sample(5))
 
 
