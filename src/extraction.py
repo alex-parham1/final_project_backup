7446e490be0e1ad77afb90abc1f699cca8f53342
sian moore <sian.moore@sainsburys.co.uk>
2022-07-07 13:46:38 +0100

    cleaned products

--- src/extraction.py
+++ src/extraction.py
@@ -1,5 +1,6 @@
 import pandas as pd
-import pandas_profiling
+
+# import pandas_profiling
 import os
 from database import get_connection, close_connection, commit_connection
 from dotenv import load_dotenv
@@ -33,26 +34,89 @@ def get_data_frame():
     return df
 
 
+def get_df_customers(df):
+    customer_df = df[["customer_name"]]
+    return customer_df
+
+
+def get_df_location(df):
+    location_df = df[["location"]]
+    return location_df
+
+
+def get_df_cards(df):
+    cards_df = df[["card_type", "card_number"]]
+    return cards_df
+
+
+def get_df_products(df):
+    products_df = df[["products"]]
+    return products_df
+
+def insert_products(connection, products_df):
+    for product in products_df:
+        
+
+def get_df_transaction(df):
+    transaction_df = df[["date", "payment_type", "total"]]
+    return transaction_df
+
+
+def get_table_df(df):
+    customer_df = get_df_customers(df)
+    location_df = get_df_location(df)
+    cards_df = get_df_cards(df)
+    products_df = get_df_products(df)
+    transaction_df = get_df_transaction(df)
+    return customer_df, location_df, cards_df, products_df, transaction_df
+
+
+
+
+def clean_products(products_df):
+    products = []
+    for order in products_df["products"]:
+        order_split = order.split(",")
+        order_split = seperate_products(order_split)
+        for item in order_split:
+            if item[0] == "Regular," or item[0] == "Large,":
+                continue
+            elif item[0] == "":
+                order_split[order_split.index(item)][0] = "Small"
+        products.append(order_split)
+    for product in products:
+        print(product)
+
+
+def seperate_products(products_df):
+    rule = [1, 2, 3]
+    products = []
+    total = int(len(products_df) / 3)
+    for value in range(total):
+        buffer = []
+        for num in rule:
+            buffer.append(products_df[0])
+            products_df.pop(0)
+        products.append(buffer)
+    return products
+
+    #     # span = 3
+    #     # ",".join(order[i:i+span])
+    #     # for i in range(0, len(order), span):
+    #     for i in range(0, len(order_split) - 1, 3) :
+    #         if i < len(order_split) - 1:
+    #             products.append(",".join([order_split[i], order_split[i+1]]))
+    #         else:
+    #             products.append(order_split[i])
+    # for product in products:
+    #     print(product)
+
+
 # Gets the data frame to check the code is working
 df = get_data_frame()
 
-"""
-WE NEED:
-
-into CARDS:
-- number
-- type
-
-into TRANSACTION:
-- timestamp
-- customer id (foreign key)
-- product id (foreign key)
-- store id (foreign key)
-- total
-
-into PRODUCTS:
-- name
-"""
+products_df = get_df_products(df)
+clean_products(products_df)
 
 
 @yaspin(text="Inserting names into DB...")
@@ -76,7 +140,7 @@ def insert_cards(connection):
                 VALUES ('{card_number}', '{card_type}')"""
         cursor = connection.cursor()
         cursor.execute(sql_query)
-        connection.commit()
+    connection.commit()
     print("Cards inserted OK")
 
 
@@ -88,13 +152,17 @@ def insert_store(connection):
         VALUES ('{store}') """
         cursor = connection.cursor()
         cursor.execute(sql_query)
-        connection.commit()
+    connection.commit()
     print("Stores inserted OK")
 
 
-insert_names(connection)
-insert_cards(connection)
-insert_store(connection)
-print(
+
+
+    # insert_names(connection)
+    # insert_cards(connection)
+    # insert_store(connection)
+    # print(
     "Note to TEAM YOGURT = if you're putting data in while testing this file - dont forget to take it out again!"
-)
+
+
+# )
