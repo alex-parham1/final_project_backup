9acece0ee139183963c536e60d832f890ec85764
alex parham <alex.parham1@sainsburys.co.uk>
2022-07-07 17:34:27 +0100

    black format

--- src/scripts/notebook_utils.py
+++ src/scripts/notebook_utils.py
@@ -1,4 +1,4 @@
-def get_labels_and_values(data:tuple):
+def get_labels_and_values(data: tuple):
     labels = []
     values = []
     for entry in data:
@@ -7,11 +7,12 @@ def get_labels_and_values(data:tuple):
         labels.append(entry[0])
         values.append(entry[1])
 
-    return labels,values
+    return labels, values
 
-def get_card_vs_cash(cards:tuple,cash:tuple):
-    combined = (cash[0]),(cards[0])
-    labels,values = get_labels_and_values(combined)
-    labels = ['cash','card']
 
-    return labels,values
+def get_card_vs_cash(cards: tuple, cash: tuple):
+    combined = (cash[0]), (cards[0])
+    labels, values = get_labels_and_values(combined)
+    labels = ["cash", "card"]
+
+    return labels, values
--- src/scripts/temp.py
+++ src/scripts/temp.py
@@ -1,7 +1,2 @@
 def test_func():
     return "hello world"
-
-
-
-
-   
