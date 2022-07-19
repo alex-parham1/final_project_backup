9374b87be867153017b6e5602fce008431028e0d
alex parham <alex.parham1@sainsburys.co.uk>
2022-07-19 17:07:52 +0100

    transactions to msql

--- src/scripts/transactions_and_baskets.py
+++ src/scripts/transactions_and_baskets.py
@@ -6,6 +6,15 @@ import sys
 from dotenv import load_dotenv
 import time
 import os
+from sqlalchemy import create_engine
+from sqlalchemy.ext.compiler import compiles
+from sqlalchemy.sql.expression import Insert
+import numpy
+
+# adds the word IGNORE after INSERT in sqlalchemy
+@compiles(Insert)
+def _prefix_insert_with_ignore(insert, compiler, **kw):
+    return compiler.visit_insert(insert.prefix_with('IGNORE'), **kw)
 
 main_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
 sys.path.append(f"{main_dir}")
@@ -14,88 +23,96 @@ from src.database import get_connection, close_connection, commit_connection
 
 con = get_connection()
 
+def df_to_sql(df:pd.DataFrame, table_name):
+    user = os.environ.get("mysql_user")
+    password = os.environ.get("mysql_pass")
+    engine = create_engine(f"mysql+pymysql://{user}:{password}@localhost:3307/thirstee")
+    db_engine = engine.execution_options(autocommit=True)
+    df.to_sql(name=table_name, con=db_engine, if_exists='append', index=False, schema='thirstee') 
+
+def df_from_sql_table(table_name):
+    user = os.environ.get("mysql_user")
+    password = os.environ.get("mysql_pass")
+    engine = create_engine(f"mysql+pymysql://{user}:{password}@localhost:3307/thirstee")
+    return pd.read_sql_table(table_name,engine)
+
+def get_store_id(store,stores:pd.DataFrame):
+    id = stores.query(f"name=='{store}'", inplace=False)
+    return str(id.values.tolist()[0][0])
+
+def get_customer_id(name,customers:pd.DataFrame):
+    #name = df["customer_name"]
+    #print(name)
+    id = customers.query(f"name=='{name}'",inplace=False)
+    return str(id.values.tolist()[0][0])
+
+# def set_foreign_keys(df: pd.DataFrame,cust:pd.DataFrame,stores:pd.DataFrame):
+#     print(df.head(10))
+#     print(df.columns)
+    
+
 
 @yaspin(text="Inserting order to DB...")
 def insert_transactions():
+    users = df_from_sql_table("customers")
+    users = users.drop_duplicates(subset="name")
+    stores = df_from_sql_table("store")
+    stores = stores.drop_duplicates(subset='name')
     trans_df = clean_the_data()
-    for order in trans_df.values.tolist():
-        sql_get_customer_id = f"""
-        SELECT customer_id FROM customers
-        WHERE name = '{order[2]}'"""
 
-        sql_get_store_id = f"""
-        SELECT store_id from store
-        WHERE name = '{order[1]}'"""
+    trans_df["customer_id"] = trans_df["customer_name"].apply(get_customer_id,args=(users,))
+    
+    trans_df["location"] = trans_df["location"].apply(get_store_id,args=(stores,))
 
-        cursor = con.cursor()
-        cursor.execute(sql_get_customer_id)
-        customer_id_t = cursor.fetchone()
-        cursor.close()
+    trans_table = trans_df.drop(columns=['customer_name','product_name','flavour','size','price','card_number'])
+    trans_table.columns=['date_time','store_id','total','payment_method','customer_id']
+    #trans_table = trans_table[['date_time','store_id','total','payment_method','customer_id']]
+    
+    trans_table = trans_table.drop_duplicates()
+    print(trans_table.head(10))
+    print('uploading transactions')
+    df_to_sql(trans_table,'transactions')
+    print('uploaded transactions')
+    
 
-        cursor = con.cursor()
-        cursor.execute(sql_get_store_id)
-        store_id_t = cursor.fetchone()
-        cursor.close()
+    #     #baskets begins here
+    #     product_ids = []
+    #     cursor = con.cursor()
+    #     sql_get_product_id = f"""
+    #     SELECT product_id FROM products 
+    #         WHERE name = '{order[3]}' AND flavour = '{order[4]}' AND size = '{order[5]}' AND price = {order[6]}"""
+    #     cursor.execute(sql_get_product_id)
+    #     prod_id_t = cursor.fetchone()
+    #     product_ids.append(prod_id_t[0])
+    #     cursor.close()
 
-        customer_id = customer_id_t[0]
-        store_id = store_id_t[0]
+    #     cursor = con.cursor()
+    #     for ID in product_ids:
+    #         sql_get_trans_id = f"""
+    #         SELECT transaction_id from transactions
+    #             WHERE customer_id = {customer_id} AND store_id = {store_id} AND date_time = '{order[0]}'"""
 
-        check_existing_trans_sql = f"""
-        SELECT transaction_id FROM transactions 
-            WHERE customer_id = {customer_id} AND store_id = {store_id} AND date_time = '{order[0]}'"""
-        cursor = con.cursor()
-        cursor.execute(check_existing_trans_sql)
-        is_exists = cursor.fetchone()
+    #         cursor.execute(sql_get_trans_id)
+    #         trans_id_t = cursor.fetchone()
+    #         transaction_id = trans_id_t[0]
 
-        if is_exists == None:
-            insert_transaction_sql = f"""
-            INSERT INTO transactions (date_time, customer_id, store_id, total, payment_method)
-                VALUES ('{order[0]}', {customer_id}, {store_id}, {order[7]}, '{order[8]}')"""
+    #         sql_check_basket_exists = f"""
+    #         SELECT transaction_id FROM basket
+    #             WHERE product_id = {ID} AND transaction_id = {transaction_id}"""
+    #         cursor.execute(sql_check_basket_exists)
+    #         is_exists = cursor.fetchone()
 
-            cursor = con.cursor()
-            cursor.execute(insert_transaction_sql)
-            cursor.close()
-            con.commit()
-        else:
-            pass
+    #         if is_exists == None:
+    #             print("Inserted basket")
+    #             sql_insert_into_basket = f"""
+    #             INSERT into basket (transaction_id, product_id)
+    #                 VALUES ({transaction_id}, {ID})"""
 
-        product_ids = []
-        cursor = con.cursor()
-        sql_get_product_id = f"""
-        SELECT product_id FROM products 
-            WHERE name = '{order[3]}' AND flavour = '{order[4]}' AND size = '{order[5]}' AND price = {order[6]}"""
-        cursor.execute(sql_get_product_id)
-        prod_id_t = cursor.fetchone()
-        product_ids.append(prod_id_t[0])
-        cursor.close()
-
-        cursor = con.cursor()
-        for ID in product_ids:
-            sql_get_trans_id = f"""
-            SELECT transaction_id from transactions
-                WHERE customer_id = {customer_id} AND store_id = {store_id} AND date_time = '{order[0]}'"""
-
-            cursor.execute(sql_get_trans_id)
-            trans_id_t = cursor.fetchone()
-            transaction_id = trans_id_t[0]
-
-            sql_check_basket_exists = f"""
-            SELECT transaction_id FROM basket
-                WHERE product_id = {ID} AND transaction_id = {transaction_id}"""
-            cursor.execute(sql_check_basket_exists)
-            is_exists = cursor.fetchone()
-
-            if is_exists == None:
-                print("Inserted basket")
-                sql_insert_into_basket = f"""
-                INSERT into basket (transaction_id, product_id)
-                    VALUES ({transaction_id}, {ID})"""
-
-                cursor.execute(sql_insert_into_basket)
-            else:
-                pass
-        cursor.close()
-        con.commit()
+    #             cursor.execute(sql_insert_into_basket)
+    #         else:
+    #             pass
+    #     cursor.close()
+    #     con.commit()
 
     print("Transactions and Baskets inserted OK")
 
