5544c94ffc5e6d27abea737c989e9136df741d42
Connor Humphries <connor.humphries@sainsburys.co.uk>
2022-07-20 15:48:33 +0100

    updated sql

--- sql/db_setup_script.sql
+++ sql/db_setup_script.sql
@@ -1,57 +1,69 @@
-CREATE table IF NOT EXISTS customers (
-	customer_id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL
-);
+-- thirstee.cards definition
 
-CREATE table IF NOT EXISTS products (
-	product_id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL,
-	flavour VARCHAR(255) NOT NULL,
-	size VARCHAR(255) NOT NULL,
-	price DECIMAL(4,2)
-);
+CREATE TABLE `cards` (
+  `card_id` int NOT NULL AUTO_INCREMENT,
+  `card_number` varchar(255) NOT NULL,
+  PRIMARY KEY (`card_id`),
+  UNIQUE KEY `card_number` (`card_number`),
+  UNIQUE KEY `card_number_2` (`card_number`),
+  UNIQUE KEY `card_number_3` (`card_number`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE table IF NOT EXISTS store (
-	store_id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(255) NOT NULL UNIQUE
-);
 
-CREATE table IF NOT EXISTS cards (
-	card_id INT AUTO_INCREMENT PRIMARY KEY,
-	card_number BIGINT NOT NULL UNIQUE,
-	card_type VARCHAR(255) NOT NULL
-);
+-- thirstee.customers definition
 
-CREATE table IF NOT EXISTS cards_customers (
-	customer_id INT NOT NULL,
-	card_id INT NOT NULL,
-	FOREIGN KEY (customer_id) 
-		REFERENCES customers (customer_id),
-	FOREIGN KEY (card_id) 
-		REFERENCES cards (card_id)
-);
+CREATE TABLE `customers` (
+  `customer_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`customer_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=20353 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
-CREATE table IF NOT EXISTS transactions (
-	transaction_id INT AUTO_INCREMENT PRIMARY KEY,
-	date_time DATETIME NOT NULL,
-	customer_id INT NOT NULL,
-	quantity INT NOT NULL,
-	store_id INT NOT NULL,
-	total DECIMAL(10,2) NOT NULL,
-	payment_method VARCHAR(255) DEFAULT 'CASH',
-	
-	FOREIGN KEY (customer_id)
-		REFERENCES customers (customer_id),
-	FOREIGN KEY (store_id)
-		REFERENCES store (store_id)
-);
-	
-CREATE table IF NOT EXISTS basket (
-	transaction_id INT NOT NULL,
-	product_id INT NOT NULL,
 
-	FOREIGN KEY (transaction_id)
-		REFERENCES transactions (transaction_id),
-	FOREIGN KEY (product_id)
-		REFERENCES products (product_id)
-);
+-- thirstee.products definition
+
+CREATE TABLE `products` (
+  `product_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `flavour` varchar(255) DEFAULT NULL,
+  `size` varchar(100) DEFAULT NULL,
+  `price` decimal(4,2) DEFAULT NULL,
+  PRIMARY KEY (`product_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=11570 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- thirstee.store definition
+
+CREATE TABLE `store` (
+  `store_id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1051 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- thirstee.transactions definition
+
+CREATE TABLE `transactions` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `date_time` varchar(255) NOT NULL,
+  `customer_id` int NOT NULL,
+  `store_id` int NOT NULL,
+  `total` decimal(10,2) NOT NULL,
+  `payment_method` varchar(255) DEFAULT 'CASH',
+  PRIMARY KEY (`transaction_id`),
+  KEY `customer_id` (`customer_id`),
+  KEY `store_id` (`store_id`),
+  CONSTRAINT `transactions_ibfk_1` FOREIGN KEY (`customer_id`) REFERENCES `customers` (`customer_id`),
+  CONSTRAINT `transactions_ibfk_3` FOREIGN KEY (`store_id`) REFERENCES `store` (`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=22550 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+-- thirstee.basket definition
+
+CREATE TABLE `basket` (
+  `transaction_id` int NOT NULL,
+  `product_id` int NOT NULL,
+  KEY `transaction_id` (`transaction_id`),
+  KEY `product_id` (`product_id`),
+  CONSTRAINT `basket_ibfk_1` FOREIGN KEY (`transaction_id`) REFERENCES `transactions` (`transaction_id`),
+  CONSTRAINT `basket_ibfk_2` FOREIGN KEY (`product_id`) REFERENCES `products` (`product_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
