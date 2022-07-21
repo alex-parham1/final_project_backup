b25da0dbac2cd0ad7848bb9812add62ced89e1ca
alex parham <alex.parham1@sainsburys.co.uk>
2022-07-21 15:27:18 +0100

    can now insert new prods agian

--- src/scripts/extraction.py
+++ src/scripts/extraction.py
@@ -21,7 +21,8 @@ def df_from_sql_table(table_name):
     host = os.environ.get("mysql_host")
     port = os.environ.get("mysql_port")
     db = os.environ.get("mysql_db")
-    engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}")
+    temp = f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}"
+    engine = create_engine(f"mysql+pymysql://alex:%^MuvtL9pg6&G7-%@localhost:3307/thirstee")
     ret = pd.read_sql_table(table_name, engine)
     engine.dispose()
     return ret
@@ -143,9 +144,9 @@ def drop_dupe_prods(df:pd.Series,prods:pd.DataFrame):
     prod = prods.query(f"name=='{name}' and size == '{size}' and flavour == '{flavour}' and price == {price}")
 
     if not prod.empty:
-        return None
+        return True
     else:
-        return df
+        return False
     
 
 def get_df_products(df):
@@ -153,8 +154,9 @@ def get_df_products(df):
     products_df = df[["product_name", "flavour", "size", "price"]]
     products_df.columns = ["name", "flavour", "size", "price"]
     products_df = products_df.drop_duplicates(ignore_index=True)
-    products_df = products_df.apply(drop_dupe_prods,args=(prods_table,),axis=1)
-    products_df = products_df.dropna()
+    products_df['duplicate'] = products_df.apply(drop_dupe_prods,args=(prods_table,),axis=1)
+    products_df = products_df[products_df["duplicate"] == False]
+    products_df = products_df.drop('duplicate',axis=1)
     print("Products DF OK")
     return products_df        
     
@@ -181,7 +183,7 @@ def df_to_sql(df, table_name):
     host = os.environ.get("mysql_host")
     port = os.environ.get("mysql_port")
     db = os.environ.get("mysql_db")
-    engine = create_engine(f"mysql+pymysql://{user}:{password}@{host}:{port}/{db}")
+    engine = create_engine(f"mysql+pymysql://alex:%^MuvtL9pg6&G7-%@localhost:3307/thirstee")
     df.to_sql(con=engine, if_exists='append', name=table_name, index=False)
     engine.dispose()
 
@@ -246,6 +248,7 @@ def etl(
     insert_cards(cards_df)
     insert_store(location_df)
     if not products_df.empty:
+        print('tried to insert :)')
         insert_products(products_df)
     else:
         print('no new products')
