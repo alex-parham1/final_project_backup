2732c1956eddc8aff25db7184591ebdca354adae
Liz_Grimshaw <elizabeth.grimshaw@sainsburys.co.uk>
2022-08-10 10:42:55 +0100

    firefix

--- src/scripts/firehose_lambda.py
+++ src/scripts/firehose_lambda.py
@@ -7,21 +7,22 @@ debug = os.environ.get("debug")
 if debug == "False":
     import firehose as fh
 else:
+    import sys
+
+    sys.path.append("../")
+    sys.path.append("../src/scripts")
     from src.scripts import firehose as fh
 
-print('Loading function')
+print("Loading function")
 
 
 def lambda_handler(event, context):
     output = []
-    print(event)
 
-    for record in event['records']:
+    for record in event["records"]:
         print("loading record")
-        print(record['recordId'])
-        payload = base64.b64decode(record['data']).decode('utf-8')
+        payload = base64.b64decode(record["data"]).decode("utf-8")
         payload = json.loads(payload)
-        print(payload)
         print("cleaning record")
 
         cleaned = fh.main_clean(payload)
@@ -30,12 +31,12 @@ def lambda_handler(event, context):
         cleaned = json.dumps(cleaned)
 
         output_record = {
-            'recordId': record['recordId'],
-            'result': 'Ok',
-            'data': base64.b64encode(cleaned.encode('utf-8')).decode('utf-8')
+            "recordId": record["recordId"],
+            "result": "Ok",
+            "data": base64.b64encode(cleaned.encode("utf-8")).decode("utf-8"),
         }
         output.append(output_record)
 
-    print('Successfully processed {} records.'.format(len(event['records'])))
+    print("Successfully processed {} records.".format(len(event["records"])))
 
-    return {'records': output}
+    return {"records": output}
